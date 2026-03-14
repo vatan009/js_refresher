600910664492b27676317ab8a86d6aaa0b18580b
Vatan009 <pawarvatan95@gmail.com>
2026-03-14 23:49:16 +0530

    spread

---
+++ js_refresher/arrayMethods/intro.js
@@ -0,0 +1,8 @@
+const arr=[1,2,3]
+console.log(arr);
+
+const newArr=arr.map((e)=>({value:e}))
+console.log(arr)
+console.log(newArr)
+
+
---
+++ js_refresher/arrayMethods/spreadOperator/spread.js
@@ -0,0 +1,9 @@
+const indoorHobbies=['chess','carrom'];
+const outdoorHobbies=['golf','cricket'];
+console.log(...indoorHobbies)
+const newArr=[...indoorHobbies]
+const newObj={...indoorHobbies};
+console.log(newArr);
+console.log(newObj);
+
+
