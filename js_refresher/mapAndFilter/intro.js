88f16e80c907413dbc3b270dff0cd75311fd5b47
Vatan009 <pawarvatan95@gmail.com>
2026-03-15 00:38:56 +0530

    Add : js_refresher > mapAndFilter,spreadOperator

---
+++ js_refresher/mapAndFilter/intro.js
@@ -0,0 +1,6 @@
+const app=[1,2,3];
+const arr = app.map((element)=>{
+  return {val:element}
+})
+console.log(app);
+console.log(arr);
---
+++ js_refresher/spreadOperator/intro.js
@@ -0,0 +1,5 @@
+const arr=[1,2,3]
+const newArr=[4,5,6];
+
+console.log([arr,newArr]);
+console.log([...arr,...newArr]);
